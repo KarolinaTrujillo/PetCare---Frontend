b85aec6589dcf8d72002cfe3ca92a424edcec9e0
JORED666 <243842@ids.upchiapas.edu.mx>
2026-03-08 23:30:45 -0600

    refactor: remove duplicate use-case files

--- src/modules/auth/usecases/logout.use-case.ts
+++
@@ -1,6 +0,0 @@
-import { authService } from '@/services/auth.service';
-
-export const logoutUseCase = (): void => {
-  authService.logout();
-  window.location.href = '/auth/login';
-};
--- src/modules/auth/usecases/register.use-case.ts
+++
@@ -1,9 +0,0 @@
-import { authService } from '@/services/auth.service';
-import { RegisterRequest, RegisterResult } from '@/modules/auth/interfaces/auth.interfaces';
-
-export const registerUseCase = async (data: RegisterRequest): Promise<RegisterResult> => {
-  const response = await authService.register(data);
-  const { user, message } = response.data;
-
-  return { user, message };
-};
